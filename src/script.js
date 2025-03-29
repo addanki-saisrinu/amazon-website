500398533b16f00c80d96d9e2c7581258a08f018
saiSrinu <saisrinuaddanki8@gmail.com>
2025-03-29 12:36:12 +0530

    delete alert

--- src/script.js
+++ src/script.js
@@ -48,6 +48,5 @@ if(NAVSEARCHBAR){
 const  CATEGORIES = document.getElementById("categories");
 if(CATEGORIES){
     CATEGORIES.addEventListener("click",function(){
-        alert("categories clicked");
     })
 }
