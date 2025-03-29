3fc316f41be0b6342100863454773094680454c8
saiSrinu <saisrinuaddanki8@gmail.com>
2025-03-29 11:34:16 +0530

    update location completed

--- src/script.js
+++ src/script.js
@@ -2,11 +2,52 @@
 const LocationButtonModal = document.getElementById("locationModalButton");
 const CloseButton = document.getElementById("modalCloseBtn");;
 const locationModal = document.getElementById("locationModal");
+const UPDATELOCATIONAPPLYBUTTON = document.getElementById("updateLocationApplyButton");
 LocationButtonModal.addEventListener("click", function () {
     locationModal.style.display = "block"
 });
 
-CloseButton.addEventListener("click", function () {
+
+
+
+CloseButton.addEventListener("click", function () { //update location close button
     locationModal.style.display = locationModal.style.display === "block" ? "none" : "block"
+});
+
+
+
+UPDATELOCATIONAPPLYBUTTON.addEventListener("click", function () {   //update location apply button
+    const LOCATIONNUMBER = document.getElementById("locationNumber").value.trim();
+    let credentials = LOCATIONNUMBER.match(/524126/gi)    //location check here
+    if (LOCATIONNUMBER) {
+        if (credentials) {
+            alert(`your location is added ${LOCATIONNUMBER}`)
+        } else {
+            alert("your location not correct")
+        }
+    } else {
+        alert("enter your pincode")
+    }
+    document.getElementById("locationNumber").value = " ";
 })
+
 //location modal end here
+
+
+const NAVSEARCHBAR = document.getElementById("navSearchBarButton");//intilize search bar
+if(NAVSEARCHBAR){
+    NAVSEARCHBAR.addEventListener("click", function () {
+        const SEARCHBAR = document.getElementById("navSearchBar").value.trim();//intialize user take value and use trim method
+        if (SEARCHBAR) { // check search bar true or not
+            alert(`${SEARCHBAR}`)
+        }
+        document.getElementById("navSearchBar").value = " "; //after search user remove old value
+    })
+}
+
+const  CATEGORIES = document.getElementById("categories");
+if(CATEGORIES){
+    CATEGORIES.addEventListener("click",function(){
+        
+    })
+}
